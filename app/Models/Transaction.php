6517c99b12d85026dc407eb4255243e6b17921da
D.putra <nyomanadi134@gmail.com>
2025-10-12 03:25:37 +0800

    menyelesaikan membuat database

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'type',
+        'amount'
+    ];
+
+    /**
+     * Get the user that owns the Transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
